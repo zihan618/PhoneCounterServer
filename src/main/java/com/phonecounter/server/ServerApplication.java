949e9226775f0bf405515b567937f39856f4d728
zihan618 <1226007238@qq.com>
2019-03-13 11:00:28 +0800

    [feat]生成war包可以部署的spring boot pom里面的注释去掉即可部署

--- src/main/java/com/phonecounter/server/ServerApplication.java
+++ src/main/java/com/phonecounter/server/ServerApplication.java
@@ -2,12 +2,29 @@ package com.phonecounter.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(ServerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
 
 }
+
+//@SpringBootApplication
+//public class ServerApplication {
+//
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ServerApplication.class, args);
+//    }
+//
+//}
